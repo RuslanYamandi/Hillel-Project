d67878f7b39cda049f81e658e2fc70508a9c4cf6
Ruslan Yamandi <ruslanjamandi13@gmail.com>
2024-05-30 22:27:03 +0200

    HW24:v2

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,18 +1,18 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import *
+import api.views as views
 
 app_name = "api"
 router = routers.DefaultRouter()
-router.register("categories", CategoryViewSet)
+router.register("categories", views.CategoryViewSet)
 
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("products/", ProductListView.as_view(), name="products-list"),
-    path("products/create", ProductCreateView.as_view(), name="products-create"),
-    path("products/<int:pk>/", ProductDetailsView.as_view(), name="products-details"),
-    path("products/<int:pk>/update", ProductUpdateView.as_view(), name="products-update"),
-    path("products/<int:pk>/delete", ProductDeleteView.as_view(), name="products-delete"),
+    path("products/", views.ProductListView.as_view(), name="products-list"),
+    path("products/create", views.ProductCreateView.as_view(), name="products-create"),
+    path("products/<int:pk>/", views.ProductDetailsView.as_view(), name="products-details"),
+    path("products/<int:pk>/update", views.ProductUpdateView.as_view(), name="products-update"),
+    path("products/<int:pk>/delete", views.ProductDeleteView.as_view(), name="products-delete"),
 ]
